3fdf5c5dbbb1cbb8dac0f368e0386787dd6ce3a6
Vincent Matthys <vmatthys@student.42.fr>
2018-01-05 17:23:44 +0100

    Function to generate data for debugging

--- tools/mixture.py
+++ tools/mixture.py
@@ -144,3 +144,69 @@ class GM(object):
                                (x-self.mu) * self.q) / self.q.sum(axis = 1)\
                                 .reshape(self.K, 1, 1)
         return
+
+def gen_arti(centerx = 1,
+     centery = 1,
+     sigma = 0.1,
+     nbex = 1000,
+     data_type = 0,
+     epsilon = 0.02,
+     cov1 = None,
+     cov2 = None):
+    """
+    Generateur de donnees,
+    :param centerx: centre des gaussiennes
+    :param centery:
+    :param sigma: des gaussiennes
+    :param nbex: nombre d'exemples
+    :param data_type: 0: melange 2 gaussiennes, 1: melange 4 gaussiennes, 2:echequier
+    :param epsilon: bruit dans les donnees
+    :return: data matrice 2d des donnnes,y etiquette des donnnees
+    """
+    if cov1 is None:
+        cov1 = np.diag([sigma, sigma])
+    if cov2 is None:
+        cov2 = np.diag([sigma, sigma])
+    if data_type == 0:
+        # melange de 2 gaussiennes
+        xpos = np.random.multivariate_normal([centerx, centerx],
+                                   cov1,
+                                   int(nbex/2))
+        xneg = np.random.multivariate_normal([-centerx, -centerx],
+                                   cov2,
+                                   int(nbex/2))
+        data = np.vstack((xpos, xneg))
+        y = np.hstack((np.ones(int(nbex/2)), -np.ones(int(nbex/2))))
+    if data_type == 1:
+        # melange de 4 gaussiennes
+        xpos = np.vstack((
+        np.random.multivariate_normal([centerx, centerx],
+                                      cov1,
+                                      int(nbex/4)),
+        np.random.multivariate_normal([-centerx, -centerx],
+                                      cov2,
+                                      int(nbex/4))
+        ))
+        xneg = np.vstack((
+        np.random.multivariate_normal([-centerx, centerx],
+                                      cov1,
+                                      int(nbex/4)),
+        np.random.multivariate_normal([centerx, -centerx],
+                                      cov2,
+                                      int(nbex/4))
+        ))
+        data = np.vstack((xpos, xneg))
+        y = np.hstack((np.ones(int(nbex/2)), -np.ones(int(nbex/2))))
+    if data_type == 2:
+        # echiquier
+        data = np.reshape(np.random.uniform(-4, 4, 2*nbex), (nbex,2))
+        y = np.ceil(data[:,0]) + np.ceil(data[:,1])
+        y = 2 * (y%2) - 1
+        # un peu de bruit
+        data[:,0] += np.random.normal(0, epsilon, nbex)
+        data[:,1] += np.random.normal(0, epsilon, nbex)
+        # on melange les donnees
+        idx = np.random.permutation((range(y.size)))
+        data = data[idx,:]
+        y = y[idx]
+    return data,y
